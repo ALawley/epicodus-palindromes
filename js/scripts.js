3405d2941bb55751b349fba0d5120f59766e58d2
Anna Lawley and Mao Thygesen <alawley1@gmail.com>
2016-01-20 14:24:32 -0800

    add palindrome check script

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 var palindromeCheck = function(userInput) {
+  var wordArray = [];
+  var reverseArray = [];
+  var reverseText = "";
+  for (var index = 1; index <= userInput.length; index +=1) {
+    wordArray.push(userInput.slice((index - 1), index));
+  };
+  for (var rindex = 1; rindex <= userInput.length; rindex +=1) {
+    reverseArray.push(wordArray.pop());
+  };
+  reverseArray.forEach(function(character) {
+    reverseText += character
+  });
+  if (reverseText === userInput) {
+    return true;
+  } else {
   return false;
+  };
 };
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe('palindromeCheck', function () {
   it("is false for words that are not the same spelled forward and back", function () {
     expect(palindromeCheck("cat")).to.equal(false);
   });
+  it("is true for words that are the same spelled forward and back", function () {
+    expect(palindromeCheck("palindromeemordnilap")).to.equal(true);
+  });
 });
